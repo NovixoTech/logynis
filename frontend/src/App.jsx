e4186766aa0ac2a23d34f3f7b7036d66ce9ca9ba
NovixoTech <novixotech@gmail.com>
2026-07-25 12:13:12 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,19 @@ import ExplainDifferently from "./pages/ExplainDifferently.jsx";
 import NoteSummarizer from "./pages/NoteSummarizer.jsx";
 import CrossSubject from "./pages/CrossSubject.jsx";
 import DebatePractice from "./pages/DebatePractice.jsx";
+import AnxietySimulator from "./pages/AnxietySimulator.jsx";
+import CommonMistakes from "./pages/CommonMistakes.jsx";
+import EssayFeedback from "./pages/EssayFeedback.jsx";
+import MoodCheckin from "./pages/MoodCheckin.jsx";
+import OnePager from "./pages/OnePager.jsx";
+import RapidRecall from "./pages/RapidRecall.jsx";
+import ReadinessScore from "./pages/ReadinessScore.jsx";
+import RevisionTimetable from "./pages/RevisionTimetable.jsx";
+import SpacedRepetition from "./pages/SpacedRepetition.jsx";
+import SpeedDrill from "./pages/SpeedDrill.jsx";
+import StudyPlan from "./pages/StudyPlan.jsx";
 import TimedMockExam from "./pages/TimedMockExam.jsx";
+import WeakTopics from "./pages/WeakTopics.jsx";
 
 function Protected({ children }) {
   const { user, loading } = useAuth();
@@ -39,14 +51,37 @@ function Routes_() {
       <Route path="/mode/:mode/chat" element={<Protected><Chat /></Protected>} />
       <Route path="/settings" element={<Protected><Settings /></Protected>} />
       <Route path="/mode/:mode" element={<Protected><ModeFeatures /></Protected>} />
+
+      {/* Study mode */}
       <Route path="/mode/study/flashcards" element={<Protected><Flashcards /></Protected>} />
-      <Route path="/mode/study/memory-aid" element={<Protected><MemoryAid /></Protected>} /> 
+      <Route path="/mode/study/memory-aid" element={<Protected><MemoryAid /></Protected>} />
       <Route path="/mode/study/concept-map" element={<Protected><ConceptMap /></Protected>} />
       <Route path="/mode/study/explain-differently" element={<Protected><ExplainDifferently /></Protected>} />
       <Route path="/mode/study/note-summarizer" element={<Protected><NoteSummarizer /></Protected>} />
       <Route path="/mode/study/cross-subject" element={<Protected><CrossSubject /></Protected>} />
-      <Route path="/mode/study/debate-practice" element={<Protected><DebatePractice /></Protected>} /> 
-      <Route path="/mode/exam prep/timedmock-exam" element={<Protected><TimedMockExam /></Protected>} /> 
+      <Route path="/mode/study/debate-practice" element={<Protected><DebatePractice /></Protected>} />
+
+      {/* Exam mode */}
+      <Route path="/mode/exam/timed-mock" element={<Protected><TimedMockExam /></Protected>} />
+      <Route path="/mode/exam/weak-topics" element={<Protected><WeakTopics /></Protected>} />
+      <Route path="/mode/exam/speed-drill" element={<Protected><SpeedDrill /></Protected>} />
+      <Route path="/mode/exam/anxiety-simulator" element={<Protected><AnxietySimulator /></Protected>} />
+      <Route path="/mode/exam/common-mistakes" element={<Protected><CommonMistakes /></Protected>} />
+      <Route path="/mode/exam/readiness-score" element={<Protected><ReadinessScore /></Protected>} />
+      <Route path="/mode/exam/study-plan" element={<Protected><StudyPlan /></Protected>} />
+
+      {/* Homework mode */}
+      <Route path="/mode/homework/essay-feedback" element={<Protected><EssayFeedback /></Protected>} />
+
+      {/* Revision mode */}
+      <Route path="/mode/revision/onepager" element={<Protected><OnePager /></Protected>} />
+      <Route path="/mode/revision/rapid-recall" element={<Protected><RapidRecall /></Protected>} />
+      <Route path="/mode/revision/revision-timetable" element={<Protected><RevisionTimetable /></Protected>} />
+      <Route path="/mode/revision/spaced-repetition" element={<Protected><SpacedRepetition /></Protected>} />
+
+      {/* Motivation mode */}
+      <Route path="/mode/motivation/mood-checkin" element={<Protected><MoodCheckin /></Protected>} />
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
@@ -54,4 +89,4 @@ function Routes_() {
 
 export default function App() {
   return <AuthProvider><Routes_ /></AuthProvider>;
-}
+  }
